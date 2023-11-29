2bc1593da2cf26c229f93fb17c02931c27c8dcee
Oliver Midbrink <oliver.midbrink@hotmail.com>
2023-11-29 20:28:15 +0100

    update

--- molytica_m/data_tools/download_scripts/download_biogrid.py
+++ molytica_m/data_tools/download_scripts/download_biogrid.py
@@ -3,6 +3,8 @@ import zipfile
 import os
 
 def download_and_unzip(url, download_path, extract_path):
+    if not os.path.exists("data"):
+        os.makedirs("data")
     """
     Download a ZIP file from the given URL and unzip it to the specified path.
     """
@@ -23,10 +25,10 @@ def download_and_unzip(url, download_path, extract_path):
 url = "https://downloads.thebiogrid.org/Download/BioGRID/Release-Archive/BIOGRID-4.4.227/BIOGRID-ALL-4.4.227.tab3.zip"
 
 # Path to save the downloaded ZIP file
-download_path = "BIOGRID-ALL-4.4.227.tab3.zip"
+download_path = "data/BIOGRID-ALL-4.4.227.tab3.zip"
 
 # Path to extract the contents of the ZIP file
-extract_path = "unzipped_biogrid"
+extract_path = "data/uzipped_biogrid"
 
 # Ensure the extraction path exists
 os.makedirs(extract_path, exist_ok=True)
