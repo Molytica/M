6ea4872ceab50bb5dcae713bc73de723ef53e9c5
Oliver Midbrink <oliver.midbrink@hotmail.com>
2023-11-30 17:33:53 +0100

    update

--- molytica_m/mtm_eval/score_molecules.py
+++ molytica_m/mtm_eval/score_molecules.py
@@ -89,15 +89,11 @@ for smiles in tqdm(smiles_list, desc="Scoring Molecules"):
     smiles_scores[smiles] = smiles_score
 
 
-print(iP_preds)
-print(iPPI_preds)
 # Correcting the indexing in the for loops
 pred_idx = 0
 for idx, graph in enumerate(iP_is_none):
     smiles = smiles_for_iP_preds[idx]
     if graph: # If there was data for this item
-        print(type(iP_preds[pred_idx]))
-        print(type(iP_tuples[idx % len(iP_tuples)][2]))
         smiles_scores[smiles] += -iP_preds[pred_idx] / 30.0 * iP_tuples[idx % len(iP_tuples)][2] # Fix indexing
         pred_idx += 1
 
