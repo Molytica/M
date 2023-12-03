389753f9d5b9e55c0d80d7958985783b92b6c361
Oliver Midbrink <oliver.midbrink@hotmail.com>
2023-12-03 12:31:08 +0100

    update

--- molytica_m/data_tools/graph_tools/append_af_metadata.py
+++ molytica_m/data_tools/graph_tools/append_af_metadata.py
@@ -3,7 +3,9 @@ from tqdm import tqdm
 import pandas as pd
 import time
 import json
+import h5py
 import sys
+import os
 
 id_mappings = pd.read_table("molytica_m/data_tools/idmapping_af_uniprot_metadata.tsv")
 af_uniprots = alpha_fold_tools.get_alphafold_uniprot_ids()
@@ -30,11 +32,40 @@ def get_ordered_unique_col_values(columns):
         unique_value_lists_dict[list(columns)[idx]] = sorted(list(value))
     return unique_value_lists_dict
 
-af_metadata = {}
-
 unique_value_lists = get_ordered_unique_col_values(columns)
 
-print(unique_value_lists[])
+def binary_encode(full_values_list, values):
+    value_to_index = {v: i for i, v in enumerate(full_values_list)}
+    binary_encoded = [0] * len(full_values_list)
+    for value in values:
+        if value in value_to_index:
+            binary_encoded[value_to_index[value]] = 1
+    return binary_encoded
 
 
+interesting_columns = ["Gene Ontology (GO)"]
+uniprot_metadata = {}
 
+for uniprot in tqdm(af_uniprots, desc="Generating Metadata file"):
+    uniprot_data = []
+    row = id_mappings[id_mappings['From'] == str(uniprot)].iloc[0]
+
+    for col in interesting_columns:
+        vals = list(set(str(row[col]).split("; ")))
+        uniprot_data += binary_encode(unique_value_lists[col], vals)
+
+    uniprot_metadata[uniprot] = uniprot_data
+
+
+save_path = "data/af_metadata/"
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+for uniprot in tqdm(list(uniprot_metadata.keys()), desc="Saving to hdf5"):
+    metadata = uniprot_metadata[uniprot]
+
+    file_name = os.path.join(save_path, f"{uniprot}_metadata.h5")
+
+    # Saving metadata to HDF5 file
+    with h5py.File(file_name, 'w') as h5file:
+        h5file.create_dataset('metadata', data=metadata)
