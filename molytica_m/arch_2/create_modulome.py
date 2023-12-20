bbd0185c311c9eceb34cef893042c26c141db11f
Oliver Midbrink <oliver.midbrink@hotmail.com>
2023-12-20 19:07:48 +0100

    update

--- molytica_m/arch_2/create_modulome.py
+++ molytica_m/arch_2/create_modulome.py
@@ -1,18 +1,43 @@
-def create_modulome(SMILES, UNIPROT_ID):
+from concurrent.futures import ProcessPoolExecutor
+from molytica_m.arch_2 import model
+
+model = model.get_model()
+
+def get_data(SMILES, uniprot_id):
+    data = ("", [], 0)
+    return data
+
+
+def expand_id_pair_to_data(arg):
+    SMILES, uniprot_id = arg
+
+    input_data = get_data(SMILES, uniprot_id)
+
+    return input_data
+
+
+def get_modulome(SMILES):
     # Your code here
+
+    uniprot_list = []
+
+    id_input_tuples = []
+
+    for uniprot_id in uniprot_list:
+        id_input_tuples.append((SMILES, uniprot_id))
+
+    with ProcessPoolExecutor() as executor:
+        data_input_tuples = executor.map(expand_id_pair_to_data, id_input_tuples)
+
+
+    modulome = model.predict(data_input_tuples)
     
-    # Placeholder for the 2x20504 vector
-    vector = [[0] * 20504, [0] * 20504]
-    
-    # Your code here
-    
-    return vector
+    return modulome
 
 def main():
     # Call create_modulome function with sample inputs
     SMILES = "C1=CC=CC=C1"
-    UNIPROT_ID = "P12345"
-    result = create_modulome(SMILES, UNIPROT_ID)
+    result = get_modulome(SMILES)
     print(result)
 
 if __name__ == "__main__":
